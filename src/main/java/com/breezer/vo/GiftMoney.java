9f52aebdf01184cd524132250eb516ee440facab
SangHoon <SangHoon@SangHoon-PC>
2018-03-24 14:32:53 +0900

    Add DB statement, Vo, dto

---
+++ src/main/java/com/breezer/vo/GiftMoney.java
@@ -0,0 +1,73 @@
+package com.breezer.vo;
+
+public class GiftMoney {
+	private long idx;
+	private String inesrtDate;
+	private String firendName;
+	private String eventKind;
+	private String eventName;
+	private long money;
+	private String memo;
+	
+	
+	public long getIdx() {
+		return idx;
+	}
+	public void setIdx(long idx) {
+		this.idx = idx;
+	}
+	public String getInesrtDate() {
+		return inesrtDate;
+	}
+	public void setInesrtDate(String inesrtDate) {
+		this.inesrtDate = inesrtDate;
+	}
+	public String getFirendName() {
+		return firendName;
+	}
+	public void setFirendName(String firendName) {
+		this.firendName = firendName;
+	}
+	public String getEventKind() {
+		return eventKind;
+	}
+	public void setEventKind(String eventKind) {
+		this.eventKind = eventKind;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	public long getMoney() {
+		return money;
+	}
+	public void setMoney(long money) {
+		this.money = money;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "GiftMoney [idx=" + idx + ", inesrtDate=" + inesrtDate + ", firendName=" + firendName + ", eventKind="
+				+ eventKind + ", eventName=" + eventName + ", money=" + money + ", memo=" + memo + ", getIdx()="
+				+ getIdx() + ", getInesrtDate()=" + getInesrtDate() + ", getFirendName()=" + getFirendName()
+				+ ", getEventKind()=" + getEventKind() + ", getEventName()=" + getEventName() + ", getMoney()="
+				+ getMoney() + ", getMemo()=" + getMemo() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/breezer/vo/UserVo.java
@@ -0,0 +1,45 @@
+package com.breezer.vo;
+
+public class UserVo {
+	private long idx;
+	private String id;
+	private String pw;
+	private String nickName;
+	
+	
+	public long getIdx() {
+		return idx;
+	}
+	public void setIdx(long idx) {
+		this.idx = idx;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "UserVo [idx=" + idx + ", id=" + id + ", pw=" + pw + ", nickName=" + nickName + "]";
+	}
+	
+	
+	
+	
+
+}
